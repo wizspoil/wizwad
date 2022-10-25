d52ff12cb93c20fc08bc816246e7b8a8aaea32e7
StarrFox <starrfox6312@gmail.com>
2022-10-25 06:53:28 -0500

    add benchmark script to tools

---
+++ tools/benchmark.py
@@ -0,0 +1,22 @@
+import tempfile
+import cProfile
+from pathlib import Path
+
+import wizwad
+
+
+ROOT = Path(__file__)
+noshare = ROOT.parent.parent / "tests" / "noshare_ki_data"
+
+with tempfile.TemporaryDirectory(ignore_cleanup_errors=True) as temp_dir_str:
+    temp_dir = Path(temp_dir_str)
+
+    extract = temp_dir / "extract"
+    extract.mkdir()
+
+    wad = wizwad.Wad(noshare / "_Shared-WorldData.wad")
+
+    with cProfile.Profile() as pr:
+        wad.extract_all(extract)
+
+    pr.print_stats("time")
