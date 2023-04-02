655ee21d1e81d65f48b590df9151c984ada87fd3
StarrFox <starrfox6312@gmail.com>
2023-04-02 06:18:04 -0500

    update

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "My Python application";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        app = pkgs.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+        };
+
+        packageName = "wizwad";
+      in {
+        packages.${packageName} = app;
+
+        defaultPackage = self.packages.${system}.${packageName};
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ poetry ];
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
+}
