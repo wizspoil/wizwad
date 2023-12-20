03ac5eb8f61ce54bacbe69695c54d43ac7d7df35
StarrFox <StarrFox6312@gmail.com>
2023-12-20 17:33:52 +0000

    update flake.nix to latest

--- flake.nix
+++ flake.nix
@@ -5,12 +5,17 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts/";
     nix-systems.url = "github:nix-systems/default";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
     self,
     flake-parts,
     nix-systems,
+    poetry2nix,
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
@@ -20,16 +25,28 @@
         pkgs,
         self',
         ...
-      }: {
-        packages.wizwad = pkgs.poetry2nix.mkPoetryApplication {
+      }: let
+        python = pkgs.python311;
+
+        poetry2nix' = poetry2nix.lib.mkPoetry2Nix {inherit pkgs;};
+      in {
+        packages.wizwad = poetry2nix'.mkPoetryApplication {
           projectDir = ./.;
+          inherit python;
         };
 
         packages.default = self'.packages.wizwad;
 
         devShells.default = pkgs.mkShell {
           name = "wizwad";
-          packages = with pkgs; [poetry black just alejandra isort python3Packages.pytest];
+          packages = with pkgs; [
+            (poetry.withPlugins (ps: with ps; [poetry-plugin-up]))
+            python
+            just
+            alejandra
+            python.pkgs.black
+            python.pkgs.isort
+          ];
         };
       };
     };
